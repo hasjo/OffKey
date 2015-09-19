f8f7f1adc1d21c82129394141ffc1d6160c2e5e6
Jordan Haskovec <jorhaskovec@gmail.com>
2015-09-19 11:39:28 -0500

    site and server are talking to each other

--- Auth/AuthServer.py
+++ Auth/AuthServer.py
@@ -1,9 +1,15 @@
 import socket
 import time
 import threading
+import redis
 
-def ThreadCommand(inpstring):
+def ThreadCommand(newSock):
     print('kicked into a thread')
+    ClientInput = newSock.recv(2048).decode('UTF-8')
+    print('Received message: ' + ClientInput)
+    newSock.sendall(bytes('THIS IS A TEST MESSAGE', 'UTF-8'))
+    newSock.shutdown(2)
+    newSock.close()
 
 def main():
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,7 +18,8 @@ def main():
 
     while True:
         (clientsocket, address) = serversocket.accept()
-        ct = threading.Thread(args = [clientsocket], target = ThreadCommand)
+        print(type(clientsocket))
+        ct = threading.Thread(args = ([clientsocket]), target = ThreadCommand)
         ct.daemon = True
         ct.start()
         print('Socket accepted from: ' + str(address))
