be7380edfe5cfdb5f625ab76b628635dde38d52f
Jordan Haskovec <jorhaskovec@gmail.com>
2015-09-22 08:04:13 -0500

    started converting auth server to REST api

--- Auth/AuthServer.py
+++ Auth/AuthServer.py
@@ -1,57 +1,20 @@
-import socket
-import time
-import threading
+from flask import Flask, render_template
 import redis
+APP = Flask(__name__)
 
-def ThreadCommand(newSock):
+@APP.route('/api/auth/<user>/<string>/', methods=['GET'])
+def auth_user(user, string):
     r = redis.StrictRedis(host='172.17.42.1', port=6379, db=0)
-    ClientInput = newSock.recv(2048).decode('UTF-8').rstrip()
-    print('Received command: ' + ClientInput)
-    if ClientInput.split(' ')[0] == 'AUTH':
-        Username = ClientInput.split(' ')[1]
-        Key = ClientInput.split(' ')[2]
-        UsernameResponse = r.get(Username + "-key")
-        if UsernameResponse != None:
-            UsernameResponse = UsernameResponse.decode()
-            KeyCompare = float(Key) * 6 + 12 / 4
-            print('comparison key is: ' + str(KeyCompare));
-            if int(KeyCompare) == int(UsernameResponse):
-                newSock.sendall(bytes(Username, 'UTF-8'))
-                r.set(Username + '-session', 'ACTIVE')
-                print('Session set for ' + Username)
-            else:
-                newSock.sendall(bytes('DENIED', 'UTF-8'))
-                print('login denied for ' + Username)
+    UsernameResponse = r.get(user + "-key")
+    if UsernameResponse != None:
+        UsernameResponse = UsernameResponse.decode()
+        Key = float(string) * 6 + 12 / 4
+        if int(UsernameResponse) == int(Key):
+            return 'SUCCESS\n'
         else:
-            newSock.sendall(bytes('DENIED', 'UTF-8'))
-            print('login denied for ' + Username)
-    if ClientInput.split(' ')[0] == 'SESS':
-        Username = ClientInput.split(' ')[1]
-        print('Session Request for: ' + Username)
-        UsernameResponse = r.get(Username + '-session')
-        print(UsernameResponse)
-        if UsernameResponse != None:
-            UsernameResponse = UsernameResponse.decode()
-            if UsernameResponse == 'ACTIVE':
-                print('Session request for ' + Username + ' successful')
-                newSock.sendall(bytes('ACTIVE', 'UTF-8'))
-            else:
-                print('Session request for ' + Username + ' unsuccessful')
-                newSock.sendall(bytes('INACTIVE', 'UTF-8'))
+            return 'FAILURE, Key is: ' + str(Key) + ' Rediskey is: ' + str(UsernameResponse) + '\n'
+    else:
+        return 'USER DOES NOT EXIST\n'
 
-    newSock.shutdown(2)
-    newSock.close()
-
-def main():
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(('0.0.0.0', 6311))
-    serversocket.listen(5)
-
-    while True:
-        (clientsocket, address) = serversocket.accept()
-        ct = threading.Thread(args = ([clientsocket]), target = ThreadCommand)
-        ct.daemon = True
-        ct.start()
-        print('Socket accepted from: ' + str(address))
-
-main()
+if __name__ == '__main__':
+    APP.run(debug=True, host='0.0.0.0', port=80)
--- Auth/docker_setup.sh
+++ Auth/docker_setup.sh
@@ -1,3 +1,3 @@
 docker build -t offkeyauth .
 
-docker run -it --rm --name auth -p 6311:6311 -v `pwd`:/workspace offkeyauth /bin/bash
+docker run -it --rm --name auth -p 80:80 -v `pwd`:/workspace offkeyauth /bin/bash
